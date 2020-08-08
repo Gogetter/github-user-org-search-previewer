030a47e375e79c6e7f2886eabb5a4024ea602029
Gogetter <euedofia@gmail.com>
2020-08-08 11:34:07 +0100

    added user name

--- src/components/avatar/AvatarDetail.js
+++ src/components/avatar/AvatarDetail.js
@@ -3,6 +3,10 @@ import React from "react"
 function AvatarDetail(props) {
     return (
         <div className="container">
+            <h3>{props.name}</h3>
+            
+            <br/>
+
             <h3>Joined GitHuB on: {props.joinedDate}</h3>
 
             <br />
